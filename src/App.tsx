11c274ad7d69bed2f32542d44836d0611ac1b635
Ricardo Carvalho <ricardoocarvalhoo10@gmail.com>
2021-06-21 11:14:01 -0300

    Frontend arquitech
    Frontend arquitech

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <div id="login">
+      <div id="top">
+        <span id="teste">LOGIN</span>
+        <span id="teste">CADASTRO</span>
+      </div>
+      <div id="nome">
+        <span>Nome de Usuário</span>
+        <span id="span">Senha</span>
+        <span id="span2">Esqueci minha senha</span>
+      </div>
+      <div id="button">
+        <button>Entrar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
